929c662c4ca62c57a3677e3ddc7e60677f8866c9
Andreu <aplana16ins@gmail.com>
2025-06-27 08:56:51 +0200

    Delete MyHealthController/SLAM/FINAL/catkin_ws directory

--- MyHealthController/SLAM/FINAL/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/MyHealthKit/MyHealthController/SLAM/FINAL/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/MyHealthKit/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/MyHealthKit/catkin_ws/devel;/opt/ros/noetic' != '' else []
